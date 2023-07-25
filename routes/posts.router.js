1980445f2e3a1f13f3391a36234fe826f3f0bceb
hikio-17 <fajri010217@gmail.com>
2023-07-25 14:16:21 +0700

    change

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -4,15 +4,15 @@ const router = express.Router()
 const postsController = require("../controller/posts.controller")
 
 router.get("/", postsController.getAll)
-router.get("/:id", postsController.getById)
-router.post("/", postsController.create)
-router.put("/:id", postsController.update)
 router.get("/about", (req, res) => {
    res.status(200).json({
       status: 'success',
       message: 'About me',
    })
 })
+router.get("/:id", postsController.getById)
+router.post("/", postsController.create)
+router.put("/:id", postsController.update)
 router.delete("/:id", postsController.delete)
 
 module.exports = router
