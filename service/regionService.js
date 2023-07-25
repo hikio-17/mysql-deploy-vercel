86b9520c36c152032a6ee7f5851348306ef33caa
hikio-17 <fajri010217@gmail.com>
2023-07-26 01:26:06 +0700

    .

--- service/regionService.js
+++ service/regionService.js
@@ -4,7 +4,7 @@ const pool = require("../database/index");
 const regionsService = {
    create: async ({ name }) => {
       const id = `region-${nanoid()}`
-      const sql = "insert into regions (id, name) values (?, ?)"
+      const sql = "insert into Regions (id, name) values (?, ?)"
       const [rows, fields] = await pool.query(sql, [id, name])   
 
       return fields;
